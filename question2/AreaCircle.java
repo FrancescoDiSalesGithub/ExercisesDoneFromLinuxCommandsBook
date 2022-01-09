9ee1d4b4e7486936275ae8b2a086b8d72fb55861
Francesco Di Sales <f.disales@hotmail.com>
2022-01-09 12:30:53 +0100

    Create AreaCircle.java

---
+++ question2/AreaCircle.java
@@ -0,0 +1,17 @@
+package question2;
+
+
+
+public class AreaCircle 
+{
+
+    public static void main(String[] args)
+    {
+        int r=2;
+        double Area = 2*3.14*r;
+
+        System.out.println("Area Circle: "+Area);
+
+    }
+    
+}
